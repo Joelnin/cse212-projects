54e822c188027dcfb4e979eb20f9837b544e9c3f
Joelnin <joelninr@gmail.com>
2025-07-16 19:30:24 -0400

    Answering "Priority Queue"

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,23 +6,53 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Create a queue with 4 diferent priorities: First, Second, Third and Last.
+    // Expected Result: First, Second, Third, last.
+    // Defect(s) Found: It took the highest number as priority, creating the list backwards. Fixed by fixing the "dequeue" function. Also, it didn't actually "removed" the item on dequeue.
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+
+        priorityQueue.Enqueue("Last", 45);
+        priorityQueue.Enqueue("First", 1);
+        priorityQueue.Enqueue("Third", 3);
+        priorityQueue.Enqueue("Second", 2);
+
+        var first = priorityQueue.Dequeue();
+        var second = priorityQueue.Dequeue();
+        var third = priorityQueue.Dequeue();
+        var last = priorityQueue.Dequeue();
+
+
+        Assert.AreEqual("First", first);
+        Assert.AreEqual("Second", second);
+        Assert.AreEqual("Third", third);
+        Assert.AreEqual("Last", last);
+
+        // Assert.Fail("Implement the test case and then remove this.");
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Enqueue every person (3) with the same priority.
+    // Expected Result: The dequeue should be in the same order as they were enqueued.
+    // Defect(s) Found: After fixing the "Dequeue" funciton, it worked.
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+
+        priorityQueue.Enqueue("Tim", 4);
+        priorityQueue.Enqueue("Bruce", 4);
+        priorityQueue.Enqueue("Joe", 4);
+
+        var first = priorityQueue.Dequeue();
+        var second = priorityQueue.Dequeue();
+        var third = priorityQueue.Dequeue();
+
+        Assert.AreEqual("Tim", first);
+        Assert.AreEqual("Bruce", second);
+        Assert.AreEqual("Joe", third);
+
+        // Assert.Fail("Implement the test case and then remove this.");
     }
 
     // Add more test cases as needed below.
